0cf063c4c775dd34d5eba50f6db05a2199d1c6b1
Egyh <124222570+Egyh@users.noreply.github.com>
2023-02-12 09:27:45 +0000

    Rspecテスト完了

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   
  def create
     @book = Book.new(book_params)
+   
     if @book.save
         flash[:notice] = "Book was successfully created"
     redirect_to book_path(@book.id)
     else
-    render :new
+     @books = Book.all
+    render :index
     end
  end
 
